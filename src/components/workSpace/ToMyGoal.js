5cf20834da0a104027c5e2e105f379a32e469fda
박채경 <bagchaegyeong@bagchaegyeong-ui-MacBookAir.local>
2023-08-25 19:44:42 +0900

    .

--- src/components/workSpace/ToMyGoal.js
+++ src/components/workSpace/ToMyGoal.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 import { changeEdit, addItem, deleteItem  } from './workSpaceSlice';
 
+// (날짜 선택 시, 해당 날짜에) 설정한 목표 조회
 function ToDoView()
 {
     const date = useSelector((state)=>(state.workSpace.date));
@@ -18,14 +19,32 @@ function ToDoView()
         <Weather/>
         <h3 className="workSpaceTitle">TO MY GOAL</h3>
         <h3>{date}</h3>
-        <img id="diaryImg" src={diaryLogo} alt="목표 작성"/>
+        {/* 여기서 설정 목표가 1. 없으면 다이어리 로고 2. 있으면 DB에서 꺼내와서 메인 목표 리스트 보여주기 */}
+        {/* -? <GoalNull/> : <GoalExist/> */}
+        </div>
+    )
+}
+// 로고 클릭하면 첫 목표 추가 화면 _ 피그마 2번째 페이지
+function GoalNull()
+{
+    return <img id="diaryImg" src={diaryLogo} alt="목표 작성"/>;
+}
+
+function GoalExist()
+{
+    return(
+        <div>
+        <h3>Goal</h3>
+        <hr className="horizonLine"></hr>
         </div>
     )
 }
 
-// function ToDoEdit()
-// {
-// }
+// 목표 추가 (메인 및 상세 목표)
+function ToDoEdit()
+{
+
+}
 
 function ToMyGoal()
 {
