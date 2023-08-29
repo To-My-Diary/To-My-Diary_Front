fe0b841962d5bfc14362be43b27773599a6db1f8
박채경 <bagchaegyeong@bagchaegyeong-ui-MacBookAir.local>
2023-08-30 03:59:51 +0900

    feat: 목표 작성 페이지 구현중

--- src/components/workSpace/ToMyGoal.js
+++ src/components/workSpace/ToMyGoal.js
@@ -1,27 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import './ToMyGoal.css'
 import diaryLogo from '../../icons/일기 작성.png'
+import trashImage from '../../icons/쓰레기통 1.png'
+import { BsSquare, BsFillCircleFill } from 'react-icons/bs';
 import Weather from "./Weather";
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import { changeEdit, addItem, deleteItem  } from './workSpaceSlice';
 
-// const goalObj
 // (날짜 선택 시, 해당 날짜에) 설정한 목표 조회
 function ToDoView()
 {
-    const date = useSelector((state)=>(state.workSpace.date));
-
     return(
-        <div id='goalWrapper' style={{backgroundImage: `url(${process.env.PUBLIC_URL + 'images/paperBackground.png'})`,
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',}}>
-        <Weather/>
-        <h3 className="workSpaceTitle">TO MY GOAL</h3>
-        <h3>{date}</h3>
+        // <div id='goalWrapper' style={{backgroundImage: `url(${process.env.PUBLIC_URL + 'images/paperBackground.png'})`,
+        // backgroundPosition: 'center',
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover',}}>
+        // <Weather/>
+        // <h3 className="workSpaceTitle">TO MY GOAL</h3>
+        // <h3>{date}</h3>
+        <div>
         {/* 여기서 설정 목표가 1. 없으면 다이어리 로고 2. 있으면 DB에서 꺼내와서 메인 목표 리스트 보여주기 */}
         {/* -? <GoalNull/> : <GoalExist/> */}
+        {/* <GoalExist/> */}
+        <GoalNull/>
         </div>
     )
 }
@@ -34,9 +36,18 @@ function GoalNull()
 function GoalExist()
 {
     return(
-        <div>
+        <div className="goalList">
+        <>
+        <BsSquare/>
         <h3>Goal</h3>
         <hr className="horizonLine"></hr>
+        <br/>
+        </>
+        <h3>detailed goal</h3>
+        <hr className="horizonLine"></hr>
+        <br/>
+        {/* for문 돌려서 id = 1 ~ 설정되어있는 목표까지 불러와서 출력 ! */}
+        {/* for문 안 실행문 */}
         </div>
     )
 }
@@ -44,23 +55,79 @@ function GoalExist()
 // 목표 추가 (메인 및 상세 목표)
 function ToDoEdit()
 {
+    const dispatch = useDispatch();
+    const [dgoal, setDgoal] = useState('')
+    const [inputs, setInputs] = useState({
+        goal:"",
+        detailGoal:[],
+    });
+    // const {goal, detailGoal} = inputs;
+    const onmainGoalHandler = (event) => {
+        console.log(`target value ${event.target.value}`)
+        setInputs((prevState) => {
+            return { ...prevState, goal: event.target.value }
+        }
+        )
+    };
+    const ondetailGoalHandler = (event) => {
+        console.log(`dgoal ${dgoal}`)
+        setInputs((prevState) => {
+            return {...prevState, detailGoal:[...prevState.detailGoal, dgoal]}
+        })
+    }
+    
 
+    return(
+        <div className="goalList">
+        <form onSubmit={(event)=>{
+            event.preventDefault();
+            dispatch(changeEdit());
+        }}>
+        {/* <BsSquare/> */} 
+        {/* 조회 페이지에서만 필요한 아이콘 */}
+        <p className="mainGoal">
+            <text id='mainGoal'>Goal</text>
+            <input className='mainGoal-input' type='text' name='mainGoal-input' onChange={onmainGoalHandler} value={inputs.goal} />
+            <BsFillCircleFill className="colorCircle"/>
+            <hr id="horizonLine"></hr>
+        </p>
+        <p className="detailGoal">
+            <text id='detailGoal'>detailed goal</text>
+            {/* <br/> */}
+            <input className='detailGoal-input' type='text' name='detailGoal-input' onChange={ondetailGoalHandler} value={inputs.dgoal} />
+            <img className="itemImage" src={trashImage} alt="쓰레기통" onClick={()=>{}} />
+            <hr id="horizonLine"></hr>
+        </p>
+        </form>
+        {/* for문 돌려서 id = 1 ~ 설정되어있는 목표까지 불러와서 출력 ! */}
+        {/* for문 안 실행문 */}
+        </div>
+    )
 }
 
 function ToMyGoal()
 {
     const dispatch = useDispatch();
     const edit = useSelector((state)=>(state.workSpace.edit));
+    const date = useSelector((state)=>(state.workSpace.date));
 
     return (
         <div className = "ToMyGoal"
+        style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/images/paperBackground.png'})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',}}
         onClick={()=>{
             // 보기 모드일 때만 div 터치 시 편집 전환
             // if(!edit){
             //     dispatch(changeEdit());
             // }
         }}>
-            <ToDoView/>
+        <Weather/>
+        <h3 className="workSpaceTitle">TO MY GOAL</h3>
+        <h3>{date}</h3>
+        <ToDoEdit/>
+            {/* <ToDoView/> */}
         </div>
     )
 }
