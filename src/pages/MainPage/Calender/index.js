da4e629f235d540f3c31940d5284c97473f5b3bd
ck <70942824+ck-pp@users.noreply.github.com>
2024-01-27 22:38:09 +0900

    fix: 응답 데이터를 날짜와 컬러리스트 형태로 변환하여 캘린더에 닷 표시

--- src/pages/MainPage/Calender/index.js
+++ src/pages/MainPage/Calender/index.js
@@ -7,54 +7,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { changeDate, changeYear, changeMonth } from 'store/slices/workSpaceSlice';
 import { saveGoalData } from 'store/slices/dataSlice';
 import { request } from 'lib/api/api_type';
+import axios from 'axios';
 
 
 function ScheduleSpace() {
-  const marks = [
-    {
-        "color": "#2ECCFA",
-        "date": 10,
-        "mainGoal": false
-    },
-{
-          "color": "#63b3ed",
-          "date": 10,
-          "mainGoal": false
-      },
-      {
-        "color": "#f21021",
-        "date": 10,
-        "mainGoal": false
-    },
-    {
-        "color": "#2ECCFA",
-        "date": 30,
-        "mainGoal": false
-    }
-  ];
-// ]
-// const marks = [
-//   {
-//     "date": 10,
-//     "colors": ['#f87171', '#63b3ed', '#f21021'], //'#32d42a', '#74ad1e'],
-//     "mainGoal": false
-//   },
-//   {
-//     "date": 30,
-//     "colors": ["#2ECCFA"],
-//     "mainGoal": false
-//   }
-// ];
     const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
     const [year, setYear] = useState(moment().format('YYYY'));
     const [month, setMonth] = useState(moment().format('MM'));
-  //   const marks = {
-  //     '2024-01-10': ['#f87171', '#63b3ed', '#f21021'], //'#32d42a', '#74ad1e'],
-  // '2024-01-15': ['#63b3ed', '#86c995'],
-  // '2024-01-22': ['#86c995'],
-  //   };
+    const [colorsByDate, setColorsByDate] = useState([]);
     const dispatch = useDispatch();
-    const groupedMarks = groupByDate(marks);
     const clickDate = ((value) => {
       const month = (value.getMonth() + 1) < 10 ? `0${value.getMonth()+1}` : `${value.getMonth()+1}`
       const day = value.getDate() < 10 ? `0${value.getDate()}` : `${value.getDate()}`
@@ -72,23 +33,38 @@ function ScheduleSpace() {
     };
     async function onMainGoalListView(props)
     {
-        const { year, month } = props;
-        request(`/goal/${year}/${month}`, options)
-        .then((data) => {
-          dispatch(saveGoalData(data))
+        // const { year, month } = props;
+        // request(`/goal/${year}/${month}`, options)
+        // .then((data) => {
+        //   dispatch(saveGoalData(data))
+        // })
+        // .catch ((error) => alert(error.message));
+
+        // TODO: 로컬 dummy data인 JSON 파일을 API endpoint로 사용하여 데이터 조회 (테스트할때만 사용)
+        axios.get('/dummy/goalTestData.json')
+        .then((res) => {
+          dispatch(saveGoalData(res))
+            console.log(res);
         })
-        .catch ((error) => alert(error.message));
     }
     async function onGoalMark(props)
     {
       {
-        const { year, month } = props;
-        request(`/calendar/goal/${year}/${month}`, options)
-        .then((data) => {
-	        console.log("data", data);
-        })
-        .catch ((error) => alert(error.message));
-      }
+      //   const { year, month } = props;
+      //   request(`/calendar/goal/${year}/${month}`, options)
+      //   .then((data) => {
+	    //     console.log("data", data);
+      //   })
+      //   .catch ((error) => alert(error.message));
+      // }
+
+      // TODO: 로컬 dummy data인 JSON 파일을 API endpoint로 사용하여 데이터 조회 (테스트할때만 사용)
+      axios.get('/dummy/markTestData.json')
+      .then((res) => {
+        setColorsByDate(transformMarks(res.data))
+          console.log(res);
+      })
+    }
     }
 return (
     <div className='scheduleWrapper'>
@@ -107,12 +83,10 @@ return (
   onClickMonth={(value, event) => {
     const year_ = clickYear(value)
     const month_ = clickMonth(value)
-    console.log(year_ + month_)
     setYear(year_)
     setMonth(month_)
     dispatch(changeYear(year_))
     dispatch(changeMonth(month_))
-    alert(`${year} ${month}`)
     onMainGoalListView({year, month});
     onGoalMark({year, month});
   }}
@@ -127,17 +101,15 @@ return (
   className="mx-auto w-full text-sm border-b"
   tileContent={({ date, view }) => {
     const formattedDate = moment(date).format('D'); // 날짜만 필요하므로 'D' 형식 사용
-    const dateInfo = groupedMarks[parseInt(formattedDate, 10)];
-    // console.log(groupedMarks)
-  // const dateInfo = marks.find(mark => mark.date === parseInt(formattedDate, 10));
+  const dateInfo = colorsByDate.find(mark => mark.date === parseInt(formattedDate, 10));
 
   if (dateInfo) {
     return (
       <div className="flex justify-center items-center absoluteDiv">
         <div className="dot-container">
-          {/* {dateInfo.colors.map((color, index) => (
+          {dateInfo.colors.map((color, index) => (
             <div key={index} className="dot" style={{ backgroundColor: color }}></div>
-          ))} */}
+          ))}
         </div>
       </div>
     );
@@ -145,23 +117,6 @@ return (
 
   return null;
 }}
-  //   const formattedDate = moment(date).format('YYYY-MM-DD');
-  //   const colors = marks[formattedDate];
-  
-  //   if (colors && colors.length > 0) {
-  //     return (
-  //       <div className="flex justify-center items-center absoluteDiv">
-  //         <div className="dot-container">
-  //           {colors.map((color, index) => (
-  //             <div key={index} className="dot" style={{ backgroundColor: color }}></div>
-  //           ))}
-  //         </div>
-  //       </div>
-  //     );
-  //   }
-  
-  //   return null;
-  // }}
 />
       <br /><br />
       </div>
@@ -169,20 +124,26 @@ return (
   );
 }
 
-const groupByDate = (marks) => {
-  const groupedMarks = {};
-  marks.forEach((mark) => {
-    const { date, color } = mark;
-    if (!groupedMarks[date]) {
-      groupedMarks[date] = [];
+// TODO: "mainGoal" 속성에 따라 투명한 정도도 고려해서 날짜-색깔 맵핑하기
+function transformMarks(marks) {
+  const transformedMarks = {};
+
+  marks.forEach(mark => {
+    const dateKey = mark.date;
+
+    if (!transformedMarks[dateKey]) {
+      transformedMarks[dateKey] = [];
     }
-    groupedMarks[date].push({ color });
+
+    transformedMarks[dateKey].push(mark.color);
   });
 
-  // Object를 배열로 변환
-  const result = Object.entries(groupedMarks).map(([date, group]) => group);
-  // console.log(result)
+  const result = Object.entries(transformedMarks).map(([date, colors]) => ({
+    date: parseInt(date),
+    colors
+  }));
+
   return result;
-};
+}
 
 export default ScheduleSpace;
