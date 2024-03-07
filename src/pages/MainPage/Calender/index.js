24fe322a4da8c5a883500f63f9ed5524f675c63f
ck <70942824+ck-pp@users.noreply.github.com>
2024-03-07 17:09:32 +0900

    Feat: 목표 삭제 및 추가 시, 캘린더 dot 표시 실시간 업데이트 되도록 수정

--- src/pages/MainPage/Calender/index.js
+++ src/pages/MainPage/Calender/index.js
@@ -1,20 +1,21 @@
 /* 메인화면 위 부분의 날짜/일정 선택 공간 (달력, 일주일 일정 등) */
 import './index.css';
-import { useState } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import moment from 'moment';
 import Calendar from 'react-calendar';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeDate, changeYear, changeMonth } from 'store/slices/workSpaceSlice';
+import { changeDate } from 'store/slices/workSpaceSlice';
 import { saveGoalData } from 'store/slices/dataSlice';
 import { request } from 'lib/api/api_type';
-import axios from 'axios';
 
 
 function ScheduleSpace() {
-    const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
+    const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD'));
     const [year, setYear] = useState(moment().format('YYYY'));
     const [month, setMonth] = useState(moment().format('MM'));
     const [colorsByDate, setColorsByDate] = useState([]);
+    const date = useSelector((state)=>(state.workSpace.date));
+    const goalList = useSelector((state) => state.tempData.goalData);
     const dispatch = useDispatch();
     const clickDate = ((value) => {
       const month = (value.getMonth() + 1) < 10 ? `0${value.getMonth()+1}` : `${value.getMonth()+1}`
@@ -28,9 +29,9 @@ function ScheduleSpace() {
     const clickMonth = ((value) => {
       return `${value.getMonth() + 1}`;
     });
-    const options = {
+    const options = useMemo(() => ({
       method: 'GET',
-    };
+    }), []);
     async function onMainGoalListView(props)
     {
         const { year, month } = props;
@@ -47,16 +48,15 @@ function ScheduleSpace() {
         //     console.log(res);
         // })
     }
-    async function onGoalMark(props)
-    {
-      {
-        const { year, month } = props;
-        request(`/calendar/goal/${year}/${month}`, options)
-        .then((data) => {
-          setColorsByDate(transformMarks(data.result, props))
-        })
-        .catch ((error) => alert(error.message));
-
+    const onGoalMark = useCallback(async (props) => {
+      const { year, month } = props;
+      try {
+          const data = await request(`/calendar/goal/${year}/${month}`, options);
+          setColorsByDate(transformMarks(data.result, props));
+      } catch (error) {
+          alert(error.message);
+      }
+  }, [options]);
 
       // TODO: 로컬 dummy data인 JSON 파일을 API endpoint로 사용하여 데이터 조회 (테스트할때만 사용)
       // axios.get('/dummy/markTestData.json')
@@ -64,20 +64,27 @@ function ScheduleSpace() {
       //   setColorsByDate(transformMarks(res.data))
       //     console.log(res);
       // })
-    }
-    }
+    useEffect(() => {
+      const dateObj = new Date(date)
+      const year_ = clickYear(dateObj)
+    const month_ = clickMonth(dateObj)
+    setYear(year_)
+    setMonth(month_)
+    onGoalMark({year, month});
+    // eslint-disable-next-line
+    }, [goalList, date, onGoalMark]);
 return (
     <div className='scheduleWrapper'>
       <br />
       <div className='calendar-container'>
       <Calendar
-  onChange={setDate} // useState로 포커스 변경 시 현재 날짜 받아오기
+  onChange={setSelectedDate} // useState로 포커스 변경 시 현재 날짜 받아오기
   formatDay={(locale, date) => moment(date).format('D')}
   formatMonth={(locale, date) => moment(date).format('MMM')}
   minDetail='year'
   next2Label={null}
   prev2Label={null}
-  value={date}
+  value={selectedDate}
   locale="en-EN"
   calendarType='gregory'
   onClickMonth={(value, event) => {
@@ -85,23 +92,18 @@ return (
     const month_ = clickMonth(value)
     setYear(year_)
     setMonth(month_)
-    dispatch(changeYear(year_))
-    dispatch(changeMonth(month_))
-    onMainGoalListView({year, month});
-    onGoalMark({year, month});
+    onMainGoalListView({year: year_, month: month_});
+    onGoalMark({year: year_, month: month_});
   }}
   onClickDay={(date_, event) => {
     date_ = clickDate(date_)
-    setDate(date_)
+    setSelectedDate(date_)
     dispatch(changeDate(date_))
-    alert(`${date_}`)
   }}
   navigationLabel={null}
   showNeighboringMonth={true} //  이전, 이후 달의 날짜 보이도록 설정
   className="mx-auto w-full text-sm border-b"
   tileContent={({ date, view }) => {
-  //   const formattedDate = moment(date).format('D'); // 날짜만 필요하므로 'D' 형식 사용
-  // const dateInfo = colorsByDate.find(mark => mark.date === parseInt(formattedDate, 10));
   const formattedDate = moment(date).format('YYYY-M-D'); // 년/월/일 형식으로 변환
 
   const dateInfo = colorsByDate.find(mark => mark.date === formattedDate);
@@ -169,3 +171,4 @@ function transformMarks(marks, props) {
 }
 
 export default ScheduleSpace;
+export { transformMarks, increaseTransparency };
